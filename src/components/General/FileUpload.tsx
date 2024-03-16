061e0e4be07254a907ffbecb682afef5e0c68398
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-03-16 20:14:23 +0200

    UI changes

--- src/components/General/FileUpload.tsx
+++ src/components/General/FileUpload.tsx
@@ -27,7 +27,6 @@ export default function FileUpload({ className }: Props) {
       }
 
       setFiles(previousFiles => {
-        console.log('Files Length ' + previousFiles.length);
         const newFiles = [
           ...previousFiles.slice(0, 2),
           ...acceptedFiles.map((file: File) => Object.assign(file))
@@ -78,26 +77,23 @@ export default function FileUpload({ className }: Props) {
         onMouseLeave={() => setIsHovered(false)}
       >
         {!hasTwoFiles && <input {...getInputProps()} />}
-        <div className=' flex  flex-col items-center justify-center text-xs text-gray-400'>
-          <div className='text-gray-400'>
-            {isDragActive ? (
-              <p>Drop the files here ...</p>
-            ) : (
-              <p>DRAG & DROP OR CLICK TO UPLOAD</p>
-            )}
-          </div>
-          <UploadSvg
-            className={`${isHovered ? 'text-blue-500 opacity-40' : 'text-blue-300 opacity-10'} absolute left-1/2 top-1/2 h-24 w-24 -translate-x-1/2 -translate-y-1/2 transform self-center  ${files.length > 0 ? 'hidden' : ''}`}
-          />
+        <div className='flex justify-center align-top text-gray-400'>
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>DRAG & DROP OR CLICK TO UPLOAD</p>
+          )}
         </div>
-
+        <UploadSvg
+          className={`${isHovered ? 'text-blue-500 opacity-15' : 'text-blue-300 opacity-10'} absolute left-1/2 top-1/2 h-24 w-24 -translate-x-1/2 -translate-y-1/2 transform self-center  ${files.length > 0 ? 'hidden' : ''}`}
+        />
         <FilesList files={files} removeFile={removeFile} />
         <motion.button
           type='submit'
           disabled={hasTwoFiles}
           whileHover={hasTwoFiles ? { scale: 1.05 } : 'none'}
           whileTap={hasTwoFiles ? { scale: 0.9 } : 'none'}
-          className={` ${hasTwoFiles ? 'instagram' : 'bg-[#e0e0e0]'}  self-center rounded-full px-8 py-2 text-lg font-bold text-white ${hasTwoFiles ? 'hover:cursor-pointer' : 'cursor-default'} `}
+          className={` ${hasTwoFiles ? 'instagram' : 'bg-[#e0e0e0]'} absolute -bottom-5  self-center rounded-full px-8 py-2 text-lg font-bold text-white ${hasTwoFiles ? 'hover:cursor-pointer' : 'cursor-default'} `}
         >
           Analyze
         </motion.button>
