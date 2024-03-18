9bd6502132a6e1e6f6cbe763e89aacb4a3191143
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-03-18 15:11:43 +0200

    UI Changes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,11 +9,11 @@ type Props = {};
 export default function Hero({}: Props) {
   return (
     <Section className='hero_section relative items-center gap-y-10'>
-      <div className='flex items-center'>
-        <h1 className='text-xl font-bold sm:text-2xl md:text-5xl'>
+      <div className='relative flex items-center'>
+        <h1 className='text-xl font-bold sm:text-2xl md:text-3xl lg:text-5xl'>
           Upload your files below
         </h1>
-        <ArrowSvg className=' ml-2 h-14 w-14 opacity-5 md:h-20  md:w-20' />
+        <ArrowSvg className='rig absolute -right-10 -top-5 h-14 w-14 translate-x-2/3  transform opacity-5 md:-right-20 md:h-20 md:w-20' />
       </div>
       <FileUpload />
     </Section>
