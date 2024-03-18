16f959e20643015017e9054feaabb4cccb924d2c
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-03-18 17:35:39 +0200

    UI Changes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,12 +3,41 @@
 import Section from '../General/Section';
 import FileUpload from '../Dropzone/Dropzone';
 import ArrowSvg from '../General/ArrowSvg';
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate
+} from 'framer-motion';
+import { useEffect } from 'react';
 
 type Props = {};
 
+const COLORS_TOP = ['#bc1888', '#CE84CF', '#e6683c'];
+
 export default function Hero({}: Props) {
+  const color = useMotionValue(COLORS_TOP[0]);
+  const backgroundImage = useMotionTemplate`radial-gradient(170% 170% at 50% 100%, #f7f6fd 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: 'easeInOut',
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'mirror'
+    });
+  }, [color]);
+
   return (
-    <Section className='hero_section relative items-center gap-y-10'>
+    <motion.section
+      className='relative mx-auto flex h-screen w-screen
+      flex-col items-center justify-center gap-y-10 p-8'
+      style={{
+        backgroundImage
+      }}
+    >
       <div className='relative flex items-center'>
         <h1 className='text-xl font-bold sm:text-2xl md:text-3xl lg:text-5xl'>
           Upload your files below
@@ -16,6 +45,6 @@ export default function Hero({}: Props) {
         <ArrowSvg className='rig absolute -right-10 -top-5 h-14 w-14 translate-x-2/3  transform opacity-5 md:-right-20 md:h-20 md:w-20' />
       </div>
       <FileUpload />
-    </Section>
+    </motion.section>
   );
 }
