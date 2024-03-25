0da7e5dd1137da44f90ba80c385011eecb9965c2
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-03-25 19:26:12 +0200

    Added page transition

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import Section from '@/components/General/Section';
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {};
 
 function page({}: Props) {
-  return <div>page</div>;
+  return (
+    <Section>
+      <Link href='/'>return home</Link>;
+    </Section>
+  );
 }
 
 export default page;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,6 +5,7 @@ import { Features } from '@/lib/data';
 import { slideInFromBottom, slideInFromTop } from '@/lib/motion';
 import { motion } from 'framer-motion';
 import LeavesSvg from '../LeavesSvg';
+import Link from 'next/link';
 
 type Props = {};
 
