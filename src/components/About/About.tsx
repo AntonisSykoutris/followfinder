873cf31e1967e1d8d21ffbc3b07993160e1f0f63
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-03-25 15:49:42 +0200

    UI changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,44 +1,23 @@
 'use client';
 
 import Section from '@/components/General/Section';
-import { LuCastle } from 'react-icons/lu';
+import { Features } from '@/lib/data';
+import { slideInFromBottom, slideInFromTop } from '@/lib/motion';
+import { motion } from 'framer-motion';
+import LeavesSvg from '../LeavesSvg';
 
 type Props = {};
 
-const features = [
-  {
-    name: 'Push to deploy',
-    description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: LuCastle
-  },
-  {
-    name: 'SSL certificates',
-    description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LuCastle
-  },
-  {
-    name: 'Simple queues',
-    description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: LuCastle
-  },
-  {
-    name: 'Advanced security',
-    description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: LuCastle
-  }
-];
-
 export default function About({}: Props) {
   return (
-    <Section className='h-[160vh] sm:h-screen'>
-      <div className='mx-auto max-w-2xl lg:text-center'>
-        <h2 className='text-base font-semibold leading-7 text-indigo-600'>
-          Deploy faster
-        </h2>
+    <Section className='relative'>
+      <LeavesSvg className='absolute -z-20 h-screen' />
+      <motion.div
+        className='mx-auto max-w-2xl lg:text-center'
+        initial='initial'
+        animate='animate'
+        variants={slideInFromTop()}
+      >
         <p className='mt-2 text-3xl font-bold tracking-tight sm:text-4xl'>
           Everything you need to{' '}
           <span className='text-instagram'>deploy your app</span>
@@ -48,10 +27,15 @@ export default function About({}: Props) {
           Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
           In mi viverra elit nunc.
         </p>
-      </div>
-      <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl'>
+      </motion.div>
+      <motion.div
+        className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl'
+        initial='initial'
+        animate='animate'
+        variants={slideInFromBottom()}
+      >
         <div className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16'>
-          {features.map(feature => (
+          {Features.map(feature => (
             <div key={feature.name} className='relative pl-16'>
               <dt className='text-base font-semibold leading-7 '>
                 <div className='instagram absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg'>
@@ -68,7 +52,7 @@ export default function About({}: Props) {
             </div>
           ))}
         </div>
-      </div>
+      </motion.div>
     </Section>
   );
 }
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,34 @@
 import { FooterNavLink } from './types';
+import { LuCastle } from 'react-icons/lu';
 
 export const MOTION_SLIDE_DEFAULT_DELAY = 0.5;
+
+export const Features = [
+  {
+    name: 'Push to deploy',
+    description:
+      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+    icon: LuCastle
+  },
+  {
+    name: 'SSL certificates',
+    description:
+      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+    icon: LuCastle
+  },
+  {
+    name: 'Simple queues',
+    description:
+      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+    icon: LuCastle
+  },
+  {
+    name: 'Advanced security',
+    description:
+      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+    icon: LuCastle
+  }
+];
 
 export const FooterCompanyLinks: FooterNavLink[] = [
   {
